5ba77e7470c43225681dfaaac42ee2748877438e
Christopher Groskopf <staringmonkey@gmail.com>
2013-02-15 14:30:59 -0600

    WE REBUILT SCRIBBLE.

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,8 @@ $(function() {
 
     var users = new Firebase(CHAT_ROOT + '/users');
     var posts = new Firebase(CHAT_ROOT + '/posts');
+    //var username = $.cookie('username');
+    //var secret = parseInt($.cookie('secret'));
 
     var $posts = $('#posts');
 
@@ -33,10 +35,51 @@ $(function() {
     });
 
     $('#post-form').submit(function() {
-        var text = $(this).find('.text').val();
+        var text = $(this).find('input[name="text"]').val();
 
-        posts.push({ 'text': text, 'author': 'onyxfish' });
+        posts.push({ 'text': text, 'author': $.cookie('username') });
             
+        return false;
+    });
+
+    function create_user(username, expires, secret) {
+        // Create user
+        users.child(username).set({ 'expires': expires, 'secret': secret });
+
+        $.cookie('username', username);
+        $.cookie('secret', secret);
+    }
+
+    $('#login-form').submit(function() {
+        var username = $(this).find('input[name="username"]').val();
+        var query = users.startAt(null, username).endAt(null, username);
+
+        query.once('value', function(snapshot) {
+            var expires = moment().add('hours', 2).valueOf();
+            var secret = Math.floor(Math.random() * 1000000).toString();
+
+            if (snapshot.val() === null) {
+                create_user(username, expires, secret);
+            }
+
+            else {
+                // Check if username is expired
+                var user = snapshot.val()[username];
+
+                if (user.secret == $.cookie('secret')) {
+                    alert('You are already using that username');
+                }
+
+                else if (moment(user.expires) > moment().subtract('hours', 2)) {
+                    create_user(username, expires, secret);
+                }
+
+                else {
+                    alert('Sorry that username is taken try another')
+                }
+            }
+        });
+
         return false;
     });
 });
